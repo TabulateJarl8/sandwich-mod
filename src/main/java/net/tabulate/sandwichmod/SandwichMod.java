0279b8568f7f3a34ef15b6ef5bb3631d35923f8c
TabulateJarl8 <tabulatejarl8@gmail.com>
2020-10-03 17:03:43 -0400

    custom item group and knife properties

--- src/main/java/net/tabulate/sandwichmod/SandwichMod.java
+++ src/main/java/net/tabulate/sandwichmod/SandwichMod.java
@@ -1,11 +1,26 @@
 package net.tabulate.sandwichmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
 import net.tabulate.sandwichmod.registry.ModItems;
 
 public class SandwichMod implements ModInitializer {
 
     public static final String MOD_ID = "sandwichmod";
+
+    public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.create(
+            new Identifier(MOD_ID, "sandwichmodItemGroup"))
+            .icon(() -> new ItemStack(ModItems.IRON_KNIFE))
+            .appendItems(stacks -> {
+                stacks.add(new ItemStack(ModItems.IRON_KNIFE));
+                stacks.add(new ItemStack(ModItems.GOLD_KNIFE));
+                stacks.add(new ItemStack(ModItems.DIAMOND_KNIFE));
+                stacks.add(new ItemStack(ModItems.NETHERITE_KNIFE));
+            })
+            .build();
 
     @Override
     public void onInitialize() {
--- src/main/java/net/tabulate/sandwichmod/registry/ModItems.java
+++ src/main/java/net/tabulate/sandwichmod/registry/ModItems.java
@@ -2,16 +2,15 @@ package net.tabulate.sandwichmod.registry;
 
 import net.tabulate.sandwichmod.SandwichMod;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
 
-    public static final Item IRON_KNIFE = new Item(new Item.Settings().group(ItemGroup.MISC));
-    public static final Item GOLD_KNIFE = new Item(new Item.Settings().group(ItemGroup.MISC));
-    public static final Item DIAMOND_KNIFE = new Item(new Item.Settings().group(ItemGroup.MISC));
-    public static final Item NETHERITE_KNIFE = new Item(new Item.Settings().group(ItemGroup.MISC));
+    public static final Item IRON_KNIFE = new Item(new Item.Settings().group(SandwichMod.ITEM_GROUP).maxCount(1).maxDamage(2));
+    public static final Item GOLD_KNIFE = new Item(new Item.Settings().group(SandwichMod.ITEM_GROUP).maxCount(1).maxDamage(1));
+    public static final Item DIAMOND_KNIFE = new Item(new Item.Settings().group(SandwichMod.ITEM_GROUP).maxCount(1).maxDamage(3));
+    public static final Item NETHERITE_KNIFE = new Item(new Item.Settings().group(SandwichMod.ITEM_GROUP).maxCount(1).maxDamage(4));
 
     public static void registerItems(){
         Registry.register(Registry.ITEM, new Identifier(SandwichMod.MOD_ID, "iron_knife"), IRON_KNIFE);
