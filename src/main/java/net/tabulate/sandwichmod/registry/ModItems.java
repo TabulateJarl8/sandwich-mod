0c089a2bced33d34778157fc0e95d22a63f31670
TabulateJarl8 <tabulatejarl8@gmail.com>
2020-10-03 16:12:24 -0400

    add every other knife type

--- src/main/java/net/tabulate/sandwichmod/registry/ModItems.java
+++ src/main/java/net/tabulate/sandwichmod/registry/ModItems.java
@@ -9,9 +9,15 @@ import net.minecraft.util.registry.Registry;
 public class ModItems {
 
     public static final Item IRON_KNIFE = new Item(new Item.Settings().group(ItemGroup.MISC));
+    public static final Item GOLD_KNIFE = new Item(new Item.Settings().group(ItemGroup.MISC));
+    public static final Item DIAMOND_KNIFE = new Item(new Item.Settings().group(ItemGroup.MISC));
+    public static final Item NETHERITE_KNIFE = new Item(new Item.Settings().group(ItemGroup.MISC));
 
     public static void registerItems(){
         Registry.register(Registry.ITEM, new Identifier(SandwichMod.MOD_ID, "iron_knife"), IRON_KNIFE);
+        Registry.register(Registry.ITEM, new Identifier(SandwichMod.MOD_ID, "gold_knife"), GOLD_KNIFE);
+        Registry.register(Registry.ITEM, new Identifier(SandwichMod.MOD_ID, "diamond_knife"), DIAMOND_KNIFE);
+        Registry.register(Registry.ITEM, new Identifier(SandwichMod.MOD_ID, "netherite_knife"), NETHERITE_KNIFE);
     }
 
 }
