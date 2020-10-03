d936b05ba7460c5bc3ae7bfacc49672c1e012d3b
TabulateJarl8 <tabulatejarl8@gmail.com>
2020-10-03 18:15:25 -0400

    add recipes

--- src/main/java/net/tabulate/sandwichmod/SandwichMod.java
+++ src/main/java/net/tabulate/sandwichmod/SandwichMod.java
@@ -2,26 +2,32 @@ package net.tabulate.sandwichmod;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import net.tabulate.sandwichmod.registry.ModItems;
 
+import java.util.Arrays;
+
 public class SandwichMod implements ModInitializer {
 
+    //Mod ID
     public static final String MOD_ID = "sandwichmod";
 
+    //Item Group
     public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.create(
             new Identifier(MOD_ID, "sandwichmodItemGroup"))
             .icon(() -> new ItemStack(ModItems.IRON_KNIFE))
             .appendItems(stacks -> {
-                stacks.add(new ItemStack(ModItems.IRON_KNIFE));
-                stacks.add(new ItemStack(ModItems.GOLD_KNIFE));
-                stacks.add(new ItemStack(ModItems.DIAMOND_KNIFE));
-                stacks.add(new ItemStack(ModItems.NETHERITE_KNIFE));
+                for (Item item : Arrays.asList(ModItems.IRON_KNIFE, ModItems.GOLD_KNIFE, ModItems.DIAMOND_KNIFE, ModItems.NETHERITE_KNIFE)) {
+                    stacks.add(new ItemStack(item));
+                }
+
             })
             .build();
 
+    //Register Items
     @Override
     public void onInitialize() {
         ModItems.registerItems();
