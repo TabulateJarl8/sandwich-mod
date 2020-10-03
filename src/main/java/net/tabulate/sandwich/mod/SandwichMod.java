7684b4018b604968ea43a064ca0162afc573df7c
TabulateJarl8 <tabulatejarl8@gmail.com>
2020-10-03 14:53:47 -0400

    add mod class

---
+++ src/main/java/net/tabulate/sandwich/mod/SandwichMod.java
@@ -0,0 +1,10 @@
+package net.tabulate.sandwich.mod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SandwichMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
